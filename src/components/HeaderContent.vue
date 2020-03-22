a362a452d94645cc111cf12c728295ae31807245
lsliangshan <loveliangshan@126.com>
2020-03-22 23:19:55 +0800

    no message

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -5,32 +5,32 @@
 </template>
 
 <script>
-  export default {
-    name: 'HeaderContent',
-    data () {
-      return {
-        title: '首页',
-        cachedTitle: ''
-      }
-    },
-    created () {
-      this.cachedTitle = this.title
-
-      setTimeout(() => {
-        this.title = '职位详情页'
-        this.cachedTitle = this.title
-      }, 3000)
+export default {
+  name: 'HeaderContent',
+  data () {
+    return {
+      title: '',
+      cachedTitle: ''
     }
+  },
+  created () {
+    this.cachedTitle = this.title
+
+    setTimeout(() => {
+      // this.title = '职位详情页'
+      this.cachedTitle = this.title
+    }, 3000)
   }
+}
 </script>
 
 <style lang="less" scoped>
-  .header_content {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-  }
+.header_content {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
 </style>
